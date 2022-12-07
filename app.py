eacacfc7563360eb303685275bf920ca4d4c84a9
Tony Long <61607926+tonylong1314520@users.noreply.github.com>
2022-12-07 05:53:47 -1000

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,155 @@
-import dash
-from dash import html
+from dash import dash_table
 from dash import dcc
-import plotly.graph_objs as go
+from dash import html
+from dash import Dash
+import numpy as np
+import pandas as pd
+import plotly.express as px
 
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
+steam_data = pd.read_csv('steam.csv')
+steam_data2 = pd.read_csv('steam2.csv')
+steam_data3 = pd.read_csv('steam3.csv')
+app = Dash(__name__)
 
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
+count_dif_game = steam_data.groupby('Name of the games')['Name of the games'].agg('count')
+
+# Average hours users spend on steam
+average = steam_data3['Average']
+
+# Tables
+raw_table = dash_table.DataTable(
+    data=steam_data.to_dict('records'),
+    columns=[{"id": i, "name": i} for i in steam_data.columns],
+    style_cell={'textAlign': 'center', 'backgroundColor': '#141414', 'font-family': 'Courier New', 'border': '#141414',
+                'width': '25%'},
+    fixed_rows={'headers': True},
+    style_header={
+        'backgroundColor': '#000010',
+        'fontWeight': 'bold',
+        'font-family': 'Courier New'
+    },
+    style_as_list_view=True,
+    style_table={'height': '400px'}
 )
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
+game_hour_table = dash_table.DataTable(
+    data=steam_data2.to_dict('records'),
+    columns=[{"id": i, "name": i} for i in steam_data2.columns],
+    style_cell={'textAlign': 'center', 'backgroundColor': '#141414', 'font-family': 'Courier New', 'border': '#141414',
+                'width': '25%'},
+    fixed_rows={'headers': True},
+    style_header={
+        'backgroundColor': '#000010',
+        'fontWeight': 'bold',
+        'font-family': 'Courier New'
+    },
+    style_as_list_view=True,
+    style_table={'height': '400px'}
 )
-
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
+player_hours_played_table = dash_table.DataTable(
+    data=steam_data3.to_dict('records'),
+    columns=[{"id": i, "name": i} for i in steam_data3.columns],
+    style_cell={'textAlign': 'center', 'backgroundColor': '#141414', 'font-family': 'Courier New', 'border': '#141414',
+                'width': '25%'},
+    fixed_rows={'headers': True},
+    style_header={
+        'backgroundColor': '#000010',
+        'fontWeight': 'bold',
+        'font-family': 'Courier New'
+    },
+    style_as_list_view=True,
+    style_table={'height': '400px'}
 )
+# free vs purchased games
+total_purchases = steam_data.groupby('Purchases')['Purchases'].agg('count')
+purch_chart = px.bar(steam_data, x="Purchases", y=steam_data.Purchases.index, color='Purchases', barmode="group",
+                     color_discrete_sequence=['#4bb2f9'], labels={'index': 'Total Count'}, template='plotly_dark',
+                     height=350)
+purch_chart.update_layout(paper_bgcolor="#121212", font_color='#84c9fb', font_family="Courier New")
 
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+# hours played vs game titles
+simplified_steam_data2 = steam_data2.nlargest(100, ['Total Hours'])
 
+titles_pie = px.pie(steam_data2.nlargest(10, ['Total Hours']), values='Total Hours', names="Unique Titles",
+                    template='plotly_dark', color_discrete_sequence=px.colors.sequential.GnBu_r, height=350)
+titles_pie.update_layout(legend={'xanchor': 'left', 'yanchor': 'top'}, font_color='#84c9fb', font_family="Courier New")
 
-########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-app.title=tabtitle
+titles_multi = px.line(x=simplified_steam_data2['Unique Titles'], y=simplified_steam_data2['Total Hours'],
+                       color=px.Constant("Average Hours"), labels=dict(x="Unique Games", y="Total Hours", color="Time"),
+                       template='plotly_dark', height=350)
+titles_multi.add_bar(x=simplified_steam_data2['Unique Titles'], y=simplified_steam_data2['Total Hours'],
+                     name="Aggregate")
+titles_multi.update_layout(font_color='#84c9fb', font_family="Courier New")
 
-########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1(myheading),
-    dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
-    ),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A('Data Source', href=sourceurl),
-    ]
-)
+# Hours vs player and average
+player_hours = px.bar(steam_data3, x=steam_data3['Total Hours'], y=steam_data3['Unique Users'], orientation='h',
+                      template='plotly_dark', height=350)
+player_hours.update_layout(font_color='#84c9fb', font_family="Courier New")
+
+# the layout
+app.layout = html.Div([
+    dcc.Tabs([
+        dcc.Tab(label='Datasets',
+                style={'backgroundColor': '#272727', 'color': '#84c9fb', 'font-family': 'Courier New'},
+                children=[html.Div([
+                    html.Div([
+                        html.H3("Raw Data Table", style={'padding-left': '45%'}),
+                        raw_table
+                    ], style={'align-items': 'center', 'height': '45%'}),
+                    html.Div([
+                        html.Div([
+                            html.H3("Game vs Time Table"),
+                            game_hour_table
+                        ], style={'flex': '45%'}),
+                        html.Div([
+                            html.H3("Player vs Hour"),
+                            player_hours_played_table
+                        ], style={'flex': '45%'})
+                    ], style={'align-items': 'center', 'display': 'flex'}),
+                ])
+                ]),
+        dcc.Tab(label='Visualizations',
+                style={'backgroundColor': '#272727', 'color': '#84c9fb', 'font-family': 'Courier New'},
+                children=[html.Div([
+                    html.Div([
+                        html.Div([
+                            html.H3("Free vs Purchased"),
+                            dcc.Graph(figure=purch_chart, style={'align-items': 'center'}),
+                        ], style={'margin': '15px', 'align-items': 'center', 'flex': '45%'}),
+                        html.Div([
+                            html.H3("Players vs Hours"),
+                            dcc.Graph(figure=player_hours),
+                        ], style={'margin': '15px', 'align-items': 'center', 'flex': '45%'}),
+                    ], style={'display': 'flex'}),
+                    html.Div([
+                        html.Div([
+                            html.H3("Game Titles vs Hours"),
+                            dcc.Graph(figure=titles_pie),
+                        ], style={'margin': '15px', 'align-items': 'center', 'flex': '45%'}),
+                        html.Div([
+                            html.H3("Game Titles vs Hours"),
+                            dcc.Graph(figure=titles_multi),
+                        ], style={'margin': '15px', 'align-items': 'center', 'flex': '45%'}),
+                    ], style={'display': 'flex'}),
+                ])
+                ]),
+        dcc.Tab(label='Game Recommendation Engine',
+                style={'backgroundColor': '#272727', 'color': '#84c9fb', 'font-family': 'Courier New'},
+                children=[html.Div([
+                    html.H1("Recommendation Generator"),
+                    html.P("Given a user_id, this engine will check all game titles played by users in the 200k sized"
+                           "database and recommends a game to the user. Note that the more games the user played, the"
+                           "more accurate the results will be."),
+                    html.Div([
+                        dcc.Input(id="get-report", type="text", placeholder="Enter a user ID"),
+                        html.Button(id='submit-button-get', children='Submit', type='submit'),
+                    ]),
+                    html.Div(id='output_div-get')
+                ], style={'align-items': 'center'})
+                ])
+    ])
+], style={'backgroundColor': '#121212', 'color': '#84c9fb', 'height': '100vh', 'width': '98vw',
+          'font-family': 'Courier New'})
+
 
 if __name__ == '__main__':
     app.run_server()
